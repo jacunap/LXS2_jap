0933ea71ed2f567db3460708df972f6672304ff2
Jorge Acuna <j.pilarte@hotmail.com>
2016-11-05 12:00:34 -0600

    src/reporte.sh

--- src/reporte.sh
+++ src/reporte.sh
@@ -4,4 +4,18 @@ REPORTE=reporte-`date +%Y%m%d`.log
 
 echo "Reporte generado por  #USER:" > reporte `date +%Y%m%d`.log
 echo " " >> $REPORTE
+echo " " >> $REPORTE
 
+echo "##############" >> $REPORTE
+echo "Interfases Disponibles en el Servidor" >> $REPORTE
+echo "##############" >> $REPORTE
+
+echo " " >> $REPORTE
+
+ifconfig >> $REPORTE
+
+echo "##############" >> $REPORTE
+echo "Puertos  Habilitados  en el Servidor" >> $REPORTE
+echo "##############" >> $REPORTE
+
+netstat -latun | grep LISTEN >> $REPORTE
